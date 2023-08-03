f04f507d21705639caa8bfa9d87e8837fb47407a
David Kunle-Dauda <davidkunledauda2003@gmail.com>
2023-08-03 15:30:25 +0100

    mjml changes

--- src/mailers/forgotPasswordMailer.js
+++ src/mailers/forgotPasswordMailer.js
@@ -1,7 +1,35 @@
+import mjml2html from "mjml";
+
 const forgotPasswordMailer = (user, code) => {
-  return `<h2> Hello ${user.username}</h2>
-<p>Here's the code to reset your password:</p>
-<p><strong>${code}</strong></p>`;
+  return mjml2html(
+    `
+    <mjml>
+      <mj-head>
+        <mj-attributes>
+          <mj-text padding="0px"></mj-text>
+          <mj-class name="blue" color="blue"></mj-class>
+          <mj-class name="big" font-size="20px"></mj-class>
+          <mj-all font-family="Arial"></mj-all>
+        </mj-attributes>
+      </mj-head>
+      <mj-body background-color="whitesmoke">
+        <mj-section background-color="whitesmoke">
+          <mj-column vertical-align="bottom">
+            <mj-text>Hello ${user.username}</mj-text>
+          </mj-column>
+        </mj-section>
+
+        <mj-section background-color="whitesmoke">
+          <mj-column>
+            <mj-text>Here's the code to reset your password:</mj-text>
+            <mj-text font-size="24px">${code}</mj-text>
+          </mj-column>
+        </mj-section>
+      </mj-body>
+    </mjml>
+  `,
+    {}
+  ).html;
 };
 
 export default forgotPasswordMailer;
