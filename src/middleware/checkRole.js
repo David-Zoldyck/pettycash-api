7dd185f3c8d1ba32da3f04b0e914d54f0187eaf2
David Kunle-Dauda <davidkunledauda2003@gmail.com>
2023-08-16 10:59:39 +0100

    fix user bug in checkRole middleware

--- src/middleware/checkRole.js
+++ src/middleware/checkRole.js
@@ -1,26 +1,26 @@
 import User from "#models/User";
 import jwt from "jsonwebtoken";
 
-export const checkRole = (...role) => async (req, res, next) => {
-  try {
+export const checkRole =
+  (...role) =>
+  async (req, res, next) => {
+    try {
+      if (!req.user) {
+        return res.status(401).send({ error: "Unauthorized" });
+      }
 
-    if (!req.user) {
-      return res.status(401).send({ error: "Unauthorized" });
+      if (!role.includes(req.user.role)) {
+        return res
+          .status(401)
+          .send({ error: "this action requires admin privileges" });
+      }
+
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Server error" });
     }
-
-    if (role.includes(user.role)) {
-      return res
-        .status(401)
-        .send({ error: "this action requires admin privileges" });
-    }
-
-    req.user = user;
-    next();
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Server error" });
-  }
-};
+  };
 
 export const checkAdmin = checkRole("admin");
 export const checkUser = checkRole("user");
