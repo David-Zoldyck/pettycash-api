63619acd4adc210a2688dd7547179996863beab3
David Kunle-Dauda <davidkunledauda2003@gmail.com>
2023-09-14 16:23:16 +0100

    Seed once for authorizers

--- src/database/seed.js
+++ src/database/seed.js
@@ -1,14 +1,15 @@
-import mongoose from "mongoose";
 import Authorizer from "#models/Authorizer";
 import seedData from "./seeds/authorizers/seedData.js";
 
-// await mongoose.connect(process.env.DB_URI);
-
 export default async function seedDatabase() {
   try {
-    await Authorizer.deleteMany();
+    // await Authorizer.deleteMany();
 
-    await Authorizer.insertMany(seedData);
+    // await Authorizer.insertMany(seedData);
+
+    seedData.forEach((data) => {
+      Authorizer.findOneOrCreate({ email: data.email }, data);
+    });
 
     console.log("Database seeded successfully");
   } catch (error) {
--- src/models/Authorizer.js
+++ src/models/Authorizer.js
@@ -9,8 +9,31 @@ const authorizerSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const Authorizer = mongoose.model("Authorizer", authorizerSchema);
-
+authorizerSchema.statics.findOneOrCreate = function (condition, doc) {
+  const self = this;
+  const newDocument = doc;
+  return new Promise((resolve, reject) => {
+    return self
+      .findOne(condition)
+      .then((result) => {
+        if (result) {
+          return resolve(result);
+        }
+        return self
+          .create(newDocument)
+          .then((result) => {
+            return resolve(result);
+          })
+          .catch((error) => {
+            return reject(error);
+          });
+      })
+      .catch((error) => {
+        return reject(error);
+      });
+  });
+};
 authorizerSchema.plugin(mongooseAutoPopulate);
+const Authorizer = mongoose.model("Authorizer", authorizerSchema);
 
 export default Authorizer;
